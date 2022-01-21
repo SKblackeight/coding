f440472a3046c0aa6ba8d3052af0289f734b0a63
aspyn-k <aspynpro@gmail.com>
2022-01-21 15:55:31 +0900

    1

---
+++ 파일탐지기초_kim.py
@@ -0,0 +1,12 @@
+file = open('t.png', 'rb')
+magic = file.read(16)
+a = magic.hex()
+print (a[0:2])
+
+b = a[0:2]
+if b == '89' or b== 'FF':
+    print ("사진파일입니다")
+#89=png, FF=jpg
+else:
+    print ("삭제해라")
+
