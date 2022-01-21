57fae863e7c8157f36ff34b4393f0c072c0c8b72
aspyn-k <aspynpro@gmail.com>
2022-01-21 16:36:02 +0900

    1

---
+++ 파일탐지함수_kim.py
@@ -0,0 +1,36 @@
+
+def hello(temp):
+    for i in temp:
+        file = open(i, 'rb')
+        magic = file.read(16)
+        b = magic.hex()
+        print (b[0:2])
+
+        c = b[0:2]
+        if c == '89' or c== 'FF':
+            print ("사진파일입니다")
+        else:
+            print ("삭제해라")
+
+
+def hello2(i):
+
+    file = open(i, 'rb')
+    magic = file.read(16)
+    b = magic.hex()
+    print (b[0:2])
+
+    c = b[0:2]
+    if c == '89' or c== 'FF':
+        print ("사진파일입니다")
+    else:
+        print ("삭제해라")
+
+
+
+if __name__ == "__main__":
+    li=['t.png', 'a.jpg']
+    hello(li)
+    hello2('t.png')
+   
+
