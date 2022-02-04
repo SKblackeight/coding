95c836129a338a25041c2a4a54869adafdfd7877
aspyn-k <aspynpro@gmail.com>
2022-02-04 15:35:20 +0900

    수정

--- 파일탐지함수_kim.py
+++ 파일탐지함수_kim.py
@@ -1,4 +1,3 @@
-
 def hello(temp):
     for i in temp:
         file = open(i, 'rb')
@@ -7,30 +6,28 @@ def hello(temp):
         print (b[0:2])
 
         c = b[0:2]
-        if c == '89' or c== 'FF':
+        if c == '89' or c== 'FF' or c=='ff':
             print ("사진파일입니다")
         else:
             print ("삭제해라")
 
 
-def hello2(i):
+# def hello2(i):
 
-    file = open(i, 'rb')
-    magic = file.read(16)
-    b = magic.hex()
-    print (b[0:2])
+#     file = open(i, 'rb')
+#     magic = file.read(16)
+#     b = magic.hex()
+#     print (b[0:2])
 
-    c = b[0:2]
-    if c == '89' or c== 'FF':
-        print ("사진파일입니다")
-    else:
-        print ("삭제해라")
+#     c = b[0:2]
+#     if c == '89' or c== 'FF':
+#         print ("사진파일입니다")
+#     else:
+#         print ("삭제해라")
 
 
 
 if __name__ == "__main__":
-    li=['t.png', 'a.jpg']
+    li=['a.png', 'b.jpg']
     hello(li)
-    hello2('t.png')
-   
-
+    # hello2('change.png')
