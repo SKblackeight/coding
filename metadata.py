43499ce67b2d903cabcf8c4826a31ac93f3c4b73
agemo4869 <agemo4869@gmail.com>
2022-02-09 11:05:07 +0900

    metadata with dict

--- metadata.py
+++ metadata.py
@@ -1,23 +1,18 @@
-# pip3 install Pillow
-
+from email import header
 from PIL import Image
 from PIL.ExifTags import TAGS
 from PIL.PngImagePlugin import PngImageFile, PngInfo
-# import time
+import os
 
-def header_check(img_files):
-    img_ck = []
-    for img_file in img_files:
-        img = Image.open(img_file)
+def header_check(path):
+    with Image.open(path["name"]) as img:
         if (img.format == "JPEG"):
-            img_ck.append(jpg_info(img))
-        elif (img.format == "PNG"):
-            img_ck.append(png_info(img))
+            path["header"] = "JPEG"
+            path["metadata"] = jpg_info(img)
+        # elif (img.format == "PNG"):
+        #     img_ck.append(png_info(img))
         else:
-            img_ck.append(None)
-        img.close()
-    for i in img_ck:
-        print (i)
+            path["header"] = "other"
 
 
 def jpg_info(jpg):
@@ -34,45 +29,28 @@ def jpg_info(jpg):
     except:
         return None
 
-def png_info(png):
-    try:
-        if "UserComment" in png.text.keys():
-            return str(png.text["UserComment"])
-        else:
-            return None
-    except:
-        return None
+def header_write(path, expiration):
+        
+    img = Image.open(path)
+    if (img.format == "JPEG"):
+        jpg_wr_meta(img,expiration)
 
-def header_write(img_files, expiration):
-    for img_file in img_files:
-        img = Image.open(img_file)
-        if (img.format == "JPEG"):
-            jpg_wr_meta(img, expiration)
-        elif (img.format == "PNG"):
-            png_wr_meta(img, expiration)
-        img.close()
+    img.close()
 
 def jpg_wr_meta(jpg, exDate):
+    
     meta_data = jpg.getexif()
     meta_data[0x9286] = exDate
-    jpg.save("out.jpg", exif = meta_data)
+    jpg.save(jpg.filename, exif = meta_data)
 
-def png_wr_meta(png, exDate):
-    meta_data = PngInfo()
-    meta_data.add_text("UserComment", exDate)
-    png.save("out.png", pnginfo = meta_data)
-
-# t = img_info()
-# time_stamp = int(time.mktime(time.strptime(t, "%Y:%m:%d %H:%M:%S")))
-# nowdate = int(time.time())
-# print(time_stamp)
-# print(nowdate)
-# if (time_stamp<nowdate):
-#     print("FALSE")
-
+def makedic(file_list):
+    dict("")
 if __name__ == "__main__":
-    # image_files = ["test1.jpg", "test2.jpg", "test3.jpeg", "test4.jpeg", "test5.png", "test6.png"]
-    image_files = ["in.jpg", "in.png"]
-    header_write(image_files, "2022:11:21 18:18:18")
-    image_files = ["out.jpg", "out.png"]
-    header_check(image_files)
+    directory = 'temp'
+    path = []
+    for i in os.listdir(directory):
+        path.append({"name":directory+i, "header":None, "metadata":None, "hash":None})
+    # print(path)
+    for file_ck in path:
+        # print(file_ck["name"])
+        header_check(file_ck)
